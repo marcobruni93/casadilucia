6c4d730b701cdf1556897477a819340f7f772f26
marcobruni93 <80783705+marcobruni93@users.noreply.github.com>
2022-05-12 17:28:30 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,14 @@
+/* menu a tendina home */
+
+
+
+document.getElementById("immagine").addEventListener("click", openTendina);
+function openTendina ()
+{
+
+    document.getElementById("menuTendina").classList.toggle("attivato");
+
+
+}
+
+
